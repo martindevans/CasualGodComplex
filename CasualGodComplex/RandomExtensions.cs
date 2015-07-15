8f3fa9c4c15bef674d485d4f6010530171f8b9a7
Pi Lanningham <QRebound@gmail.com>
2015-07-14 23:13:53 -0400

    Adds a normal distribution generator
     - This takes advantage of the second Box-Muller constant, might be slightly faster in long sequences.

--- CasualGodComplex/RandomExtensions.cs
+++ CasualGodComplex/RandomExtensions.cs
@@ -1,11 +1,38 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CasualGodComplex
 {
     static class RandomExtensions
     {
         /// <summary>
-        /// Generates a single value from a normal distribution
+        /// Generates a sequence of values from a normal distribution, using Box-Muller
+        /// https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
+        /// </summary>
+        /// <param name="random">A (uniform) random number generator</param>
+        /// <param name="standardDeviation">The standard deviation of the distribution</param>
+        /// <param name="mean">The mean of the distribution</param>
+        /// <returns>A normally distributed value</returns>
+        public static IEnumerable<float> NormallyDistributedSingles(this Random random, float standardDeviation, float mean)
+        {
+            while(true)
+            {
+                var u1 = random.NextDouble(); //these are uniform(0,1) random doubles
+                var u2 = random.NextDouble();
+                
+                var x1 = Math.Sqrt(-2.0 * Math.Log(u1));
+                var x2 = 2.0 * Math.PI * u2;
+                var z1 = x1 * Math.Sin(x2); //random normal(0,1)
+                yield return (float)(z1 * standardDeviation + mean);
+                var z2 = x1 * Math.Cos(x2);
+                yield return (float)(z2 * standardDeviation + mean);
+            }
+        }
+        
+        /// <summary>
+        /// Generates a single value from a normal distribution, using Box-Muller
+        /// https://en.wikipedia.org/wiki/Box%E2%80%93Muller_transform
         /// </summary>
         /// <param name="random">A (uniform) random number generator</param>
         /// <param name="standardDeviation">The standard deviation of the distribution</param>
@@ -13,12 +40,7 @@ namespace CasualGodComplex
         /// <returns>A normally distributed value</returns>
         public static float NormallyDistributedSingle(this Random random, float standardDeviation, float mean)
         {
-            double u1 = random.NextDouble(); //these are uniform(0,1) random doubles
-            double u2 = random.NextDouble();
-
-            double randStdNormal = Math.Sqrt(-2.0 * Math.Log(u1)) * Math.Sin(2.0 * Math.PI * u2); //random normal(0,1)
-
-            return (float)(randStdNormal * standardDeviation + mean);
+            return random.NormallyDistributedSingles(standardDeviation, mean).First();
         }
 
         public static float NormallyDistributedSingle(this Random random, float standardDeviation, float mean, float min, float max)
